bdc02ffae12124effe09aa58b5774f6ca834ea47
Lori0292 <562332@my.cuhsd.org>
2025-06-03 10:53:56 -0700

    Update PantryItems.java

--- PantryItems.java
+++ PantryItems.java
@@ -14,4 +14,8 @@ public class PantryItems extends Foods {
     public int getExpireYear(){
        return expireYear;
     }
+
+    public int setYearBought(int year){
+        expireYear = year;
+    }
 }
