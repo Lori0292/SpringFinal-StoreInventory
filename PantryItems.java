5ec42c2b878d4c32b9ebd68bf51f5f0a61513dc6
Lori0292 <562332@my.cuhsd.org>
2025-06-03 01:10:13 -0700

    Create PantryItems.java

---
+++ PantryItems.java
@@ -0,0 +1,17 @@
+public class PantryItems extends Foods {
+   //instance variable for PantryItems object
+   private int expireYear;
+
+   public PantryItems(String name, int amount, int yearBought){
+       super(name, amount);
+       expireYear = yearBought + 10;
+   }
+   
+    /**
+    Returns the int expireYear - expiration date of a given pantryItem object.
+    @return names;
+    */
+    public int getExpireYear(){
+       return expireYear;
+    }
+}
